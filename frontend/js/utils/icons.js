55d5635c9ef7c3fe7993af7fb7fdc4384d125187
Daniel Limon C <cervantesdaniellimon50m@gmail.com>
2025-11-11 10:49:55 -0600

    Pagina base de login y reesctructuracion de carpetas

---
+++ frontend/js/utils/icons.js
@@ -0,0 +1,21 @@
+export const sunIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
+     viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <circle cx="12" cy="12" r="5"></circle>
+  <path d="M12 1v2m0 18v2m11-11h-2M3 12H1m16.95 6.95-1.414-1.414M6.464 6.464 5.05 5.05M18.364 5.636l-1.414 1.414M6.464 17.536 5.05 18.95"></path>
+</svg>`;
+
+export const moonIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
+     viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+</svg>`;
+
+export const chatIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
+     viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+</svg>`;
